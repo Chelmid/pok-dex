4ecd0f4d903e759945b7c3fdf5c6bcab1cf2a057
Michel <lo.chelmi@gmail.com>
2020-12-12 21:01:59 +0100

    ajouter des commentaire poour le connection server

--- src/server/server.js
+++ src/server/server.js
@@ -3,14 +3,14 @@ const app = express()
 const port = 3000
 
 const mongoose = require('mongoose');
-
+//permet de connecter a mongoose
 mongoose.Promise = global.Promise
-
+// server
 mongoConnectionString = "mongodb+srv://root:root@cluster1.s0peq.mongodb.net/test"
-
+//promise de la connection
 mongoose.connect(mongoConnectionString, {useNewUrlParser: true, useUnifiedTopology: true}) .then(() => console.log("MongoDB Connected..."))
 .catch((err) => console.log(err));
-
+//connection
 const connection = mongoose.connection;
 
 console.log(connection)
@@ -18,7 +18,7 @@ console.log(connection)
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
+//connection du serveur dnas le port 3000
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
