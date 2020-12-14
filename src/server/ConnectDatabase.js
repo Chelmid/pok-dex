3ad0da1edc355c780b5c9be459f98059d60e0b77
Michel <lo.chelmi@gmail.com>
2020-12-14 17:47:05 +0100

    checking connexion mangodb

--- src/server/ConnectDatabase.js
+++ src/server/ConnectDatabase.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
-//permet de connecter a mongoose
-mongoose.Promise = global.Promise
+
 // server
 mongoConnectionString = "mongodb+srv://root:root@cluster1.s0peq.mongodb.net/test"
 //promise de la connection
-mongoose.connect(mongoConnectionString, {useNewUrlParser: true, useUnifiedTopology: true}) .then(() => console.log("MongoDB Connected..."))
+const connect = mongoose.connect(mongoConnectionString, {useNewUrlParser: true, useUnifiedTopology: true}) .then(() => console.log("MongoDB Connected ..."))
 .catch((err) => console.log(err));
-//connection
-const connection = mongoose.connection;
 
-console.log(connection)
-
-module.exports = connection
+module.exports = connect
--- src/server/server.js
+++ src/server/server.js
@@ -2,15 +2,35 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const connection = require('./ConnectDatabase')
-// connection
-connection
+const connect = require('./ConnectDatabase')
+const mongoose = require('mongoose');
 
-console.log(connection)
+if(mongoose.connection.readyState === 2){
+    console.log(mongoose.connection.readyState , 'connecting ...')
+mongoose.connection.on('connected', () => {
+        console.log(mongoose.connection.readyState , 'connected ...')
+        app.get('/', (req, res) => {
+            res.redirect('/sucessConnection')
+        })
+    
+})};
 
-app.get('/', (req, res) => {
+
+app.get('/save', (req, res) => {
     res.send('Hello World!')
+    const schema = new mongoose.Schema({ name: 'string', size: 'string' });
+    const Tank = mongoose.model('Tank', schema);
+    console.log(Tank)
+    Tank.create({ size: 'small' }, (err, small) => {
+        if (err) return handleError(err);
+        // saved!
+    });
 })
+
+app.get('/sucessConnection', (req, res) => {
+    res.send('connection success server')
+})
+
 //connection du serveur dnas le port 3000
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
