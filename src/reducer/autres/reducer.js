2ccc0315b4da976173953a3381bb8eeded7e173e
Michel <lo.chelmi@gmail.com>
2020-12-26 14:24:50 +0100

    modification du reducer

--- src/reducer/autres/reducer.js
+++ src/reducer/autres/reducer.js
@@ -2,10 +2,9 @@ import { INIT } from './constant-actions'
 
 // initialisation des states
 let stateInit = {
-    dragons: [
-        "Apalala",
-        "Balaur",
-        "Bollaa"
+    Pokemon: [
+        idPokemon = '',
+        apiPokemon = 'https://pokeapi.co/api/v2/pokemon?limit=20'
     ],
 }
 let reducer = (state = stateInit, action = {}) => {
