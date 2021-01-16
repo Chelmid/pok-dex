2fb23ce35415c2ae7a48139ce803d96fab4b918e
Michel <lo.chelmi@gmail.com>
2021-01-16 13:16:40 +0100

    login fields

--- src/components/connect/login.js
+++ src/components/connect/login.js
@@ -2,11 +2,27 @@ import React from 'react';
 
 const login = () => {
     
+    const handleSubmit = () => (
+        console.log('click')
+    )
 
     return (
 
         <div>
             <h2>Login</h2>
+
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Your Email:
+                    <input type="text" name="email" />
+                </label>
+                <label>
+                    Your Name:
+                    <input type="text" name="name" />
+                </label>
+                
+                <button type="submit">Valider</button>
+            </form>
         </div>
 
     )
