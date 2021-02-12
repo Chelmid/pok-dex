9e60cf7c656689eab5b7585ad73c80fba818e8c5
Michel <lo.chelmi@gmail.com>
2021-02-12 17:03:38 +0100

    mise à jour

--- client/src/reducer/pokemonReducer/actions-type.js
+++
@@ -1,8 +0,0 @@
-import { INIT, SET_POKEMON_ID, STATUS_ONE_POKEMON } from './constant-actions';
-
-//action et payload
-export const setPokemon = (payload) => { return { type: SET_POKEMON_ID, payload }  } ;
-
-export const init = (payload) => { return { type: INIT, payload }  } ;
-
-export const setStatusOnePokemon = (payload) => { return { type: STATUS_ONE_POKEMON, payload }  } ;
--- client/src/reducer/pokemonReducer/constant-actions.js
+++
@@ -1,5 +0,0 @@
-//les constant des action
-
-export const INIT  = 'INIT'
-export const SET_POKEMON_ID  = 'SET_POKEMON_ID'
-export const STATUS_ONE_POKEMON  = 'STATUS_ONE_POKEMON'
